d127e14ef8ba87b620f51259084a53e10b67c920
Sushant Narvekar <115461235+Sushant2518@users.noreply.github.com>
2026-07-04 18:27:54 +0530

    Time: 4 ms (20.08%), Space: 43.9 MB (13.17%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,32 +1,50 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        char[] ans = new char[word1.length()+word2.length()];
+        // Brute force approach 
+        String ans = "";
         int i = 0;
-        int j = 0;
-        int k = 0;
 
-        while(i <= word1.length()-1 && i <= word2.length()-1){
-            ans[k] = word1.charAt(i);
-            k++;
-            i++;
+        while(i<=word1.length()-1 || i<=word2.length()-1){
+            if(i<=word1.length()-1){
+                ans+=word1.charAt(i);
+            }
 
-            ans[k] = word2.charAt(j);
-            k++;
-            j++;
-        }
-
-        while(i <= word1.length()-1){
-            ans[k] = word1.charAt(i);
-            k++;
+            if(i<=word2.length()-1){
+                ans+=word2.charAt(i);
+            }
             i++;
         }
+        return ans;
+        
 
-        while(j <= word2.length()-1){
-            ans[k] = word2.charAt(j);
-            k++;
-            j++;
-        }
+        // Approach 3
+        // char[] ans = new char[word1.length()+word2.length()];
+        // int i = 0;
+        // int j = 0;
+        // int k = 0;
 
-        return String.valueOf(ans);
+        // while(i <= word1.length()-1 && i <= word2.length()-1){
+        //     ans[k] = word1.charAt(i);
+        //     k++;
+        //     i++;
+
+        //     ans[k] = word2.charAt(j);
+        //     k++;
+        //     j++;
+        // }
+
+        // while(i <= word1.length()-1){
+        //     ans[k] = word1.charAt(i);
+        //     k++;
+        //     i++;
+        // }
+
+        // while(j <= word2.length()-1){
+        //     ans[k] = word2.charAt(j);
+        //     k++;
+        //     j++;
+        // }
+
+        // return String.valueOf(ans);
     }
 }
