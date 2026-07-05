caa0f9d7e3b1de6e9f1a96e714dad669e4261e9b
Sushant Narvekar <115461235+Sushant2518@users.noreply.github.com>
2026-07-05 17:13:01 +0530

    Time: 1 ms (94.55%), Space: 43.8 MB (16.68%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -1,30 +1,46 @@
 class Solution {
+    // Approach 2
     public String gcdOfStrings(String str1, String str2) {
-        int minLength = Math.min(str1.length(), str2.length());
-
-        for(int len = minLength; len>=1; len--){
-            String candidate = str1.substring(0, len);
-
-            if(isDivisor(str1, candidate) && isDivisor(str2, candidate)){
-                return candidate;
-            }
+        if(!(str1 + str2).equals(str2 + str1)){
+            return "";
         }
-        return "";
+
+        int gcd = greatestCommonDivisor(str1.length(), str2.length());
+
+        return str1.substring(0, gcd); 
     }
 
-    private boolean isDivisor(String str, String candidate) {
-        if(str.length() % candidate.length() != 0){
-            return false;
-        }
-
-        int repeat = str.length()/candidate.length();
-
-        StringBuilder sb = new StringBuilder();
-
-        for(int i=0; i<=repeat-1; i++){
-            sb.append(candidate);
-        }
-
-        return sb.toString().equals(str);
+    private int greatestCommonDivisor(int a, int b) {
+        return b == 0 ? a : greatestCommonDivisor(b, a % b);
     }
+
+    // Approach 1 
+    // public String gcdOfStrings(String str1, String str2) {
+    //     int minLength = Math.min(str1.length(), str2.length());
+
+    //     for(int len = minLength; len>=1; len--){
+    //         String candidate = str1.substring(0, len);
+
+    //         if(isDivisor(str1, candidate) && isDivisor(str2, candidate)){
+    //             return candidate;
+    //         }
+    //     }
+    //     return "";
+    // }
+
+    // private boolean isDivisor(String str, String candidate) {
+    //     if(str.length() % candidate.length() != 0){
+    //         return false;
+    //     }
+
+    //     int repeat = str.length()/candidate.length();
+
+    //     StringBuilder sb = new StringBuilder();
+
+    //     for(int i=0; i<=repeat-1; i++){
+    //         sb.append(candidate);
+    //     }
+
+    //     return sb.toString().equals(str);
+    // }
 }
