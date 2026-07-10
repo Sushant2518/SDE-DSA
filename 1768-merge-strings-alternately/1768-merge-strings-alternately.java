f3166040f67713fc6ab1c86edf5d4130291aae45
Sushant Narvekar <115461235+Sushant2518@users.noreply.github.com>
2026-07-10 14:55:51 +0530

    Time: 1 ms (96%), Space: 43.1 MB (63.61%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,85 +1,22 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        //Approach 1 : Brute force approach string concatenation T.C = o((m+n)^2) S.C = o(m+n)
-        // String ans = "";
-        // int i = 0;
-
-        // while(i<=word1.length()-1 || i<=word2.length()-1){
-        //     if(i<=word1.length()-1){
-        //         ans+=word1.charAt(i);
-        //     }
-
-        //     if(i<=word2.length()-1){
-        //         ans+=word2.charAt(i);
-        //     }
-        //     i++;
-        // }
-        // return ans;
-
-        // Approach 2 : Using StringBuilder T.C = o(m+n) S.c = o(m+n)
-        // StringBuilder ans = new StringBuilder();
-        // int i = 0;
-
-        // while(i<=word1.length()-1 || i<=word2.length()-1){
-        //     if(i<=word1.length()-1){
-        //         ans.append(word1.charAt(i));
-        //     }
-
-        //     if(i<=word2.length()-1){
-        //         ans.append(word2.charAt(i));
-        //     }
-        //     i++;
-        // }
-        // return ans.toString();
-
-        // Approach 3 using char[] T.C = o(m+n) S.c = o(m+n)
-        char[] ans = new char[word1.length()+word2.length()];
+        char[] charArray = new char[word1.length()+word2.length()];
         int i = 0;
         int k = 0;
 
-        while(i<=word1.length()-1 || i<=word2.length()-1){
+        while(i<=word1.length()-1 || i<=word2.length()-1) {
             if(i<=word1.length()-1){
-                ans[k] = word1.charAt(i);
+                charArray[k] = word1.charAt(i);
                 k++;
             }
 
-            if(i<=word2.length()-1){
-                ans[k] = word2.charAt(i);
+            if (i<=word2.length()-1){
+                charArray[k] = word2.charAt(i);
                 k++;
             }
             i++;
         }
-        return String.valueOf(ans);
 
-
-        // below code shorten in apporch 3
-        // char[] ans = new char[word1.length()+word2.length()];
-        // int i = 0;
-        // int j = 0;
-        // int k = 0;
-
-        // while(i <= word1.length()-1 && i <= word2.length()-1){
-        //     ans[k] = word1.charAt(i);
-        //     k++;
-        //     i++;
-
-        //     ans[k] = word2.charAt(j);
-        //     k++;
-        //     j++;
-        // }
-
-        // while(i <= word1.length()-1){
-        //     ans[k] = word1.charAt(i);
-        //     k++;
-        //     i++;
-        // }
-
-        // while(j <= word2.length()-1){
-        //     ans[k] = word2.charAt(j);
-        //     k++;
-        //     j++;
-        // }
-
-        // return String.valueOf(ans);
+        return String.valueOf(charArray);
     }
 }
