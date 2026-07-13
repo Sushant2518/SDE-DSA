a8e18035555224d95ea5ce48661bc622ebf085f2
Sushant Narvekar <115461235+Sushant2518@users.noreply.github.com>
2026-07-13 09:45:43 +0530

    Time: 1 ms (98.83%), Space: 47.4 MB (94.36%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        if (n == 0){
+        if(n==0){
             return true;
         }
 
         int count = 0;
-        
+
         for(int i=0; i<=flowerbed.length-1; i++){
             if(flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) && (i == flowerbed.length-1 ||flowerbed[i+1] == 0)){
                 count++;
@@ -15,25 +15,7 @@ class Solution {
                 }
                 i++;
             }
-            
         }
         return false;
     }
-    // public boolean canPlaceFlowers(int[] flowerbed, int n) {
-    //     if (n == 0){
-    //         return true;
-    //     }
-        
-    //     for(int i=0; i<=flowerbed.length-1; i++){
-    //         if(flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) && (i == flowerbed.length-1 ||flowerbed[i+1] == 0)){
-    //             flowerbed[i] = 1;
-    //             n--;
-
-    //             if(n < 0){
-    //                 return true;
-    //             }
-    //         }
-    //     }
-    //     return false;
-    // }
 }
