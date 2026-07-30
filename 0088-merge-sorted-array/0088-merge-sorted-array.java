a649bf13962ef73b907f9ff0925882a515c39647
Sushant Narvekar <115461235+Sushant2518@users.noreply.github.com>
2026-07-30 22:53:22 +0530

    Time: 2 ms (24.54%), Space: 43.4 MB (95.53%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,24 +1,23 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+        // Step 1: Copy nums2 into nums1
+        for (int i = 0; i < n; i++) {
+            nums1[m + i] = nums2[i];
+        }
 
-        while(m > 0 && n > 0){
-            if(nums1[m-1] > nums2[n-1]){
-                nums1[m+n-1] = nums1[m-1];
-                m--;
-            } else {
-                nums1[m+n-1] = nums2[n-1];
-                n--;
+        // Step 2: Bubble Sort
+        int size = m + n;
+
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - 1 - i; j++) {
+                if (nums1[j] > nums1[j + 1]) {
+
+                    // Swap
+                    int temp = nums1[j];
+                    nums1[j] = nums1[j + 1];
+                    nums1[j + 1] = temp;
+                }
             }
-        }
-
-        while(n > 0){
-            nums1[m+n-1] = nums2[n-1];
-            n--;
-        }
-
-        while(m > 0){
-            nums1[m+n-1] = nums1[m-1];
-            m--;
         }
     }
 }
